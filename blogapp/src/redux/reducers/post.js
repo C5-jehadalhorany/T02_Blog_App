3e262b47fd7ba7fabdb0935bae8e60a89bbf38bb
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-20 02:17:21 +0300

    Eidt in redux for post

--- blogapp/src/redux/reducers/post.js
+++ blogapp/src/redux/reducers/post.js
@@ -1,1 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
+export const postSlice = createSlice({
+    name: "post",
+    initialState: {
+        post: [],
+    },
+    reducers: {
+        setpost: (state, action) => {
+            state.post = action.payload;
+        },
+        addPost: (state, action) => {
+            state.post.push(action.payload);
+
+        },
+        updatePost: (state, action) => {
+            state.post = state.post.map((element, index) => {
+                if (element.id === action.payload.id) {
+                    return element.id = action.payload
+                } else {
+                    return element
+                }
+            });
+        },
+        deletepost: (state, action) => {
+            state.post = state.post.filter((element) => {
+                // console.log(action.payload  , "....." , element.id);  
+                return element.id != action.payload;
+            });
+        }
+    }
+
+
+})
+
+
+export const { setpost,
+    addPost,
+    updatePost,
+    deletepost,
+} = postSlice.actions
+
+export default postSlice.reducer
