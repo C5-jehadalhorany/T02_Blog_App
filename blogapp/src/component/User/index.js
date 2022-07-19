268ecb5eec6ac38c470879939226806a9b0ab2e3
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-20 02:16:45 +0300

    add the user and just tist render the name and Email for the user

--- blogapp/src/component/User/index.js
+++ blogapp/src/component/User/index.js
@@ -1,9 +1,38 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 
 
 
 
 const User = () => {
+    const [usershow, setUsershow] = useState([])
+    const dispatch = useDispatch()
+
+
+
+    const userRen = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`).then((result) => {
+            setUsershow(result.data)
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+
+    useEffect(() => {
+        userRen()
+    }, [])
+
+    return (<div>
+        {usershow && usershow.map((element, index) => {
+            return (<div>
+                <h1>{element.username}</h1>
+                <h1>{element.email}</h1>
+            </div>)
+        })}
+    </div>)
 
 }
 
