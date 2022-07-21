03771c36d7823c88af82f6c34725b961c4f08351
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-21 22:54:24 +0300

    clean code no log and test it

--- blogapp/src/component/Login/index.js
+++ blogapp/src/component/Login/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { setislog,setislogout } from "../../redux/reducers/login"
+import { setislog, setislogout } from "../../redux/reducers/login"
 import { useNavigate } from "react-router-dom";
 
 
@@ -18,50 +18,31 @@ const Login = () => {
     const state = useSelector((state) => {
         return {
             login: state.login.login,
-            logout:state.login.logout
-            
+            logout: state.login.logout
         };
     });
-
-    console.log(state);
 
     const cilcktologin = () => {
         axios.get(`https://jsonplaceholder.typicode.com/users?username=${name}
         `).then((result) => {
             if (result.data[0].email == email) {
                 dispatch(setislog(result.data[0].id))
-                // dispatch(setislogout(false))
                 navigate("/");
-                console.log(result.data[0]);
                 setIslogin(result.data)
             }
-            // console.log(result.data.length);
-            // console.log(result.data);
         }).catch((err) => {
             console.log(err);
         })
-
-    }
-
-    console.log(name);
-
-
-    // useEffect(() => {
-    //     console.log(islogin);
-    // }, [console.log(islogin)])
-    useEffect(()=>{
+    };
+    useEffect(() => {
         cilcktologin()
-    },[])
-
+    }, [])
     return <div>
-
         <h1>Login</h1>
         <input placeholder="enter UserName" type="text" onChange={(e) => { setName(e.target.value) }} />
         <input placeholder="enter Email" type="text" onChange={(e) => { setEmail(e.target.value) }} />
         <button onClick={() => { cilcktologin() }}>Log in</button>
     </div>
-
-
 }
 
 
