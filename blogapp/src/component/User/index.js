18d17eed0183575d87f36e7c5b90f1af6ca20987
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-21 22:56:39 +0300

    Eidt and add the function for  user

--- blogapp/src/component/User/index.js
+++ blogapp/src/component/User/index.js
@@ -2,19 +2,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-
-
+import { users } from "../../redux/reducers/user"
+import { setpost } from '../../redux/reducers/post';
 
 
 const User = () => {
-    const [usershow, setUsershow] = useState([])
     const dispatch = useDispatch()
 
+    const [userp, setP] = useState([])
+    const [profile, setProfile] = useState([])
 
+    const { userId, login, user, post } = useSelector((state) => {
+        return {
+            userId: state.login.userId,
+            login: state.login.login,
+            user: state.user.user,
+            post: state.post.post
+        };
+    });
+
+    let conter2 = user.length
+    let conter = post.length
+
+    const userprofil = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/albums`).then((result) => {
+            setProfile(result.data)
+            // console.log(result.data); object
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
 
     const userRen = () => {
         axios.get(`https://jsonplaceholder.typicode.com/users`).then((result) => {
-            setUsershow(result.data)
+
+            dispatch(users(result.data))
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    const userpost = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts`).then((result) => {
+            dispatch(setpost(result.data.reverse()))
+
+            // console.log(result.data)
         }).catch((err) => {
             console.log(err);
         })
@@ -23,18 +55,29 @@ const User = () => {
 
     useEffect(() => {
         userRen()
+        userprofil()
+        userpost()
     }, [])
 
     return (<div>
-        {usershow && usershow.map((element, index) => {
-            return (<div>
-                <h1>{element.username}</h1>
-                <h1>{element.email}</h1>
-            </div>)
+        {user && user.map((element, index) => {
+            // conuterforpost = 0
+            // conuterforprofile=0
+            return <div key={element.id}>
+                <div key={element.id}>
+                    <tr>
+                        <th>{element.username}</th>
+                        <th>{element.username}</th>
+                    </tr>
+                </div>
+            </div>
+        })}
+        {post && post.map((element) => {
+            return <div key={element.id}>
+            </div>
         })}
     </div>)
 
 }
-
 
 export default User
