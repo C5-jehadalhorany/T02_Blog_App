b34067616d0eb9843909a4dda03ad68e3568f8e0
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-21 22:59:52 +0300

    Eidt and change some name and add

---
+++ blogapp/src/redux/reducers/user.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const user = createSlice({
+    name: "user",
+    initialState: {
+        user: [],
+    },
+    reducers: {
+        users: (state, action) => {
+            state.user = action.payload;
+            // console.log(action.payload);
+        },
+    },
+});
+
+export const { users } = user.actions;
+export default user.reducer;
