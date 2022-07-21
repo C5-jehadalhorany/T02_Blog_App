a95ddd9446f8d9183a1bf35cd16f3ad14ee9de82
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-21 22:57:22 +0300

    clean code no logs and test all reducers

--- blogapp/src/redux/reducers/login.js
+++ blogapp/src/redux/reducers/login.js
@@ -3,24 +3,21 @@ export const isloginSlice = createSlice({
     name: "login",
     initialState: {
         login: false,
-        logout:true,
-        userId:localStorage.getItem("userId")||""
+        logout: true,
+        userId: localStorage.getItem("userId") || ""
     },
     reducers: {
         setislog: (state, action) => {
             state.login = true
-            state.userId=action.payload
-            localStorage.setItem("userId",action.payload)
+            state.userId = action.payload
+            localStorage.setItem("userId", action.payload)
         },
-        setislogout:(state, action) => {
+        setislogout: (state, action) => {
             state.logout = false
-            state.userId=""
+            state.userId = ""
             localStorage.clear()
         },
-
-
-        
     }
 })
-export const {setislog,setislogout}=isloginSlice.actions
+export const { setislog, setislogout } = isloginSlice.actions
 export default isloginSlice.reducer
