17a930effd00d4440b8089d4469a9e82d35775bf
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-21 22:59:06 +0300

    store add inside

--- blogapp/src/redux/store.js
+++ blogapp/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import isloginSlice from "./reducers/login";
 import  postSlice  from "./reducers/post";
+import userSlice from "./reducers/user";
 
 export default configureStore({
     reducer: {
         login: isloginSlice,
-        post: postSlice
+        post: postSlice,
+        user: userSlice
     }
-})
+}); 
