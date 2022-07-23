0d85c1f5f045c64e797fb8e794f6a4c982e4a361
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-23 14:42:44 +0300

    add navgate

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import User from './component/User';
 import { Routes, Route } from "react-router-dom";
 import ProFileForUser from './component/Profile';
 import './App.css';
+import { useNavigate } from "react-router-dom";
+
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App  "  >
+
       <Navbars />
+      <h1>blog-add</h1>
+      <a onClick={()=>{
+        navigate("/login")
+      }}>Login</a>
 
       <Routes className="main  container">
         <Route path="/post" element={<PostForRen />} />
